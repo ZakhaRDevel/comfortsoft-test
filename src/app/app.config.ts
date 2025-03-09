6bfa4c382268fc61f16c775e7253d275cf55a00c
ez <ez@gmail.com>
2025-03-10 02:16:53 +0300

    chore(app): provide http

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,14 @@ import {provideRouter} from '@angular/router';
 import {NG_EVENT_PLUGINS} from '@taiga-ui/event-plugins';
 import {routes} from './app.routes';
 import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideAnimations(),
-    NG_EVENT_PLUGINS,]
+  providers: [
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(),
+    NG_EVENT_PLUGINS,
+  ]
 };
